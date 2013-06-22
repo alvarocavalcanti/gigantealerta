f90e3efbfa2a40559871289a2f71589856dac6c9
NavaL <mnandri@thoughtworks.com>
2013-06-22 15:10:36 +0300

    cleaning js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,34 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function(){
+    var map;
+    function setupMap() {
+      if (navigator.geolocation) {
+          map= navigator.geolocation.getCurrentPosition(initialize);
+        } else {
+          alert("Geolocation not available");
+        }
+      }   
+
+    function initialize(position) {
+        var mapOptions = {
+            center: new google.maps.LatLng(position.coords.latitude,  position.coords.longitude),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+        marker = new google.maps.Marker( { position: mapOptions.center, map: map, title: "You are here" });    
+        return map     
+    }
+
+    function updateMap(points){
+        var i;
+        for (i=0; i<points.length; i++){
+            marker = new google.maps.Marker({ position: new google.maps.LatLng(points.latitude,  pooints.longitude), map: map});
+        }
+    }
+    
+    google.maps.event.addDomListener(window, 'load', setupMap);
+});
