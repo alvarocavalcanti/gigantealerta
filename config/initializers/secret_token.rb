08c4c8fee8dae59ea2e6b6e7e1cb0353b2bc6b1a
Sri Prasanna <k.sriprasanna@gmail.com>
2013-06-22 13:17:42 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vinegar::Application.config.secret_token = 'dea92f8dddb1c0aa04e653ee9f6fd52770bd0aa0ceca5ef8309bf4d942719cbd0cf8804ee475fb950c472fb76a8ee29f3f34dec6b8c694a9646e9cdd746702c8'
